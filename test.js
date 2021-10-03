68b20901a48f0125c4be627ef9dc986ae3c6cb47
Larkin <code.larkin@gmail.com>
2021-10-02 20:46:07 -0400

    cleanup

--- test.js
+++ test.js
@@ -22,30 +22,15 @@ const ipfsAddOptions = {
   cidVersion: 1,
   hashAlg: 'sha2-256'
 }
-////for await (const file of ipfs.addAll(globSource('./docs', globSourceOptions), addOptions)) {
 const numNFTs = 2 //1000
 let results = []
-//for await (const res of ipfs.addAll(["0.png", "1.png"])) {
 for (let i = 0; i < numNFTs; i++) {
-    console.log(`Processing image ${i}`)
+    console.log(`Processing image ${i}...`)
     const fname = './images/' + i + '.png'
-    //console.log(fs.existsSync(fname))
-    //
-    //const readStream = (await fetch(fname)).response.body.getReader()
-    //
-    //const readStream = fs.createReadStream(fname)
-    //console.log(readStream)
-    //
-    //const fr = new global.FileReader()
-    //fr.readAsBinaryString(fname)
-    //const readStream0 = ipfs.types.Buffer.from(btoa(fr.result),"base64")
-
     const img = fs.readFileSync(fname)
-    //console.log(readStream)
-    console.log(img)
     const res = await ipfs.add(img, ipfsAddOptions)
-    console.log(res)
-    results.push[res]
+    //console.log(res)
+    results.push(res)
 }
 
 console.log(results)
